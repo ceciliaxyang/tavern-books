60ca4687f6b474deccdf5296eea3416367399fbe
James Maynard <jameskirkmaynard@gmail.com>
2016-12-28 10:15:08 -0800

    add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+books = Book.create([
+  {
+    :name => 'Sample Number One',
+    :title => 'Sample Number One',
+    :description => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    :author => 'James Maynard',
+    :translator => 'James Maynard',
+    :isbn => '1234567890123',
+    :price => 12.00,
+    :paypalCart => 'asdflkjdflksadfasdf',
+    :paypalBuy => 'asdfasdflkjasdflkajsdf',
+    :pages => '20',
+    :quote => 'This is a quote, it is really good',
+    :quoter => 'Some Guy',
+    :aboutTranslator => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    :aboutAuthor => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    :isbn_hardcover => '1234567890123',
+    :price_hardcover => '12.95',
+    :pages_hardcover => '50',
+    :paypalBuy_hardcover => 'asdfasdfasdfasdfasdf',
+    :paypalCart_hardcover => 'asdfkasdfkasdhtkasdlfk'
+  },
+  {
+    :name => 'Sample Number Two',
+    :title => 'Sample Number Two',
+    :description => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    :author => 'James Maynard',
+    :translator => 'James Maynard',
+    :isbn => '1234567890123',
+    :price => 12.00,
+    :paypalCart => 'asdflkjdflksadfasdf',
+    :paypalBuy => 'asdfasdflkjasdflkajsdf',
+    :pages => '20',
+    :quote => 'This is a quote, it is really good',
+    :quoter => 'Some Guy',
+    :aboutTranslator => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    :aboutAuthor => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    :isbn_hardcover => '1234567890123',
+    :price_hardcover => '12.95',
+    :pages_hardcover => '50',
+    :paypalBuy_hardcover => 'asdfasdfasdfasdfasdf',
+    :paypalCart_hardcover => 'asdfkasdfkasdhtkasdlfk'
+  }
+]);
